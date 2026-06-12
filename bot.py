899f2ef7be1fb343af035b8b0d43d1ff9da743f6
Timofey Kostikov <orion4605@gmail.com>
2026-06-12 13:48:30 +0300

    fix

---
+++ bot.py
@@ -0,0 +1,45 @@
+import asyncio
+import logging
+
+from aiogram import Dispatcher, Bot
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+
+from config import create_session
+from database.models import init_db
+from handlers.start import router_start
+from handlers.admin.create_lottery import router as create_lottery_router
+from handlers.admin.refund import router as refund_router
+
+import utils.shared_state as shared_state
+
+
+async def main(config):
+    logging.basicConfig(level=logging.INFO)
+
+    bot = Bot(
+        token=config.BOT_TOKEN,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML),
+        session=create_session(),
+        storage=MemoryStorage()
+    )
+
+    dp = Dispatcher()
+
+    dp.include_routers(
+        router_start,
+        create_lottery_router,
+        refund_router,
+    )
+
+    await init_db()
+
+    # 🔥 CRITICAL SECTION
+    shared_state.BOT = bot
+    shared_state.EVENT_LOOP = asyncio.get_running_loop()
+    shared_state.READY = True
+
+    print("Bot started")
+
+    await dp.start_polling(bot)
