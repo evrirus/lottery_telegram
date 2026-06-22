32e800d5894ca1b283441fc9fbbded3e0b2cb67e
Timofey Kostikov <orion4605@gmail.com>
2026-06-23 00:24:13 +0300

    fix

--- handlers/replenish.py
+++ handlers/replenish.py
@@ -14,7 +14,7 @@ router = Router()
 @router.callback_query(F.data == "replenish")
 async def replenish_handler(cbd: CallbackQuery):
     user = await UserService.get_user(cbd.message.chat.id)
-    balance = user.balance.quantize(Decimal("0.01"))
+    balance = user.balance.quantize(Decimal("1"))
     await cbd.message.answer(
         f"Ваш баланс: {balance}р\n\n",
         reply_markup=to_replenish_keyboard()
