3b9554addab145f6a7d2789e65598b54c0ff2883
Timofey Kostikov <orion4605@gmail.com>
2026-07-02 14:58:00 +0300

    fix (test)

--- handlers/inline_mode/referral.py
+++ handlers/inline_mode/referral.py
@@ -2,20 +2,21 @@ import uuid
 
 from aiogram import Router
 from aiogram.types import InlineQueryResultArticle, InputTextMessageContent, InlineQuery
+from aiogram.utils.deep_linking import create_start_link
 
 router = Router()
 
 @router.inline_query()
 async def inline_query_handler(query: InlineQuery):
     text = query.query.strip()
-
+    link = await create_start_link(query.bot, f'{query.from_user.id}')
     results = [
         InlineQueryResultArticle(
             id=str(uuid.uuid4()),
             title=f"Выслать приглашение",
             description="Нажмите, чтобы отправить реферальную ссылку",
             input_message_content=InputTextMessageContent(
-                message_text=f'Приглашаю тебя в бот лотерея: <a href="tg://resolve?domain=gotgm_bot&start={query.from_user.id}">Переходи по ссылке</a>'
+                message_text=f'Приглашаю тебя в бот лотерея: <a href="{link}">Переходи по ссылке</a>'
             )
         ),
         InlineQueryResultArticle(
